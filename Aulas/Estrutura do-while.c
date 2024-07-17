ec4a5743f39cc792016e6f80409b671388b91652
Marcos Vinicios Fernandes <128204238+mrkn03@users.noreply.github.com>
2024-07-17 14:47:35 -0300

    Add files via upload

---
+++ Aulas/Estrutura do-while.c
@@ -0,0 +1,6 @@
+#Sintaxe                                #Regra
+do                                      V: Volta
+{                                       F: Pula fora
+    comando 1
+    comando 2
+} while(condição);
